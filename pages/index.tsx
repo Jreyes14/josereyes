305cd514ab2d70ebc5bfa1b7231975560d39f5f9
Jose Reyes <josereyesb95@gmail.com>
2022-01-16 13:29:36 -0300

    Primer commit

---
+++ pages/index.tsx
@@ -0,0 +1,11 @@
+import type { NextPage } from 'next'
+
+const Home: NextPage = () => {
+  return (
+    <div>
+      <h1>Mi nombre es José Reyes Bastidas</h1>
+    </div>
+  )
+}
+
+export default Home
